ae6c27552c3418ab56217945b8a862a2ca6bccf7
afmelo92 <andre.fabian.melo@gmail.com>
2020-04-14 17:29:59 -0400

    Add navigation system

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Text } from 'react-native';
 
 import Background from '~/components/Background';
+
 // import { Container } from './styles';
 
-export default function Dashboard() {
+export default function Profile() {
   return (
     <Background>
-      <Text>Dashboard</Text>
+      <Text>MEU PERFIL</Text>
     </Background>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,59 @@
 import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { useSelector } from 'react-redux';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function Routes() {
   const signedIn = useSelector((state) => state.auth.signed);
 
   return (
-    <Stack.Navigator>
+    <>
       {signedIn ? (
-        <Stack.Screen
-          name="Home"
-          component={Dashboard}
-          options={{ headerShown: false }}
-        />
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name === 'Agendamentos') {
+                iconName = 'event';
+              } else {
+                iconName = 'person';
+              }
+
+              return <Icon name={iconName} size={20} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            keyboardHidesTabBar: true,
+            activeTintColor: '#fff',
+            inactiveTintColor: 'rgba(255,255,255, 0.6)',
+            style: {
+              backgroundColor: '#8d41a8',
+            },
+          }}
+        >
+          <Tab.Screen
+            name="Agendamentos"
+            component={Dashboard}
+            options={{ headerShown: false }}
+          />
+          <Tab.Screen
+            name="Meu Perfil"
+            component={Profile}
+            options={{ headerShown: false }}
+          />
+        </Tab.Navigator>
       ) : (
-        <>
+        <Stack.Navigator>
           <Stack.Screen
             name="SignIn"
             component={SignIn}
@@ -31,8 +64,8 @@ export default function Routes() {
             component={SignUp}
             options={{ headerShown: false }}
           />
-        </>
+        </Stack.Navigator>
       )}
-    </Stack.Navigator>
+    </>
   );
 }
