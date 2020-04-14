854c3ce9947ed44d36c953783993f352e8d165c4
afmelo92 <andre.fabian.melo@gmail.com>
2020-04-14 11:30:39 -0400

    Add redux configuration

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import './config/ReactotronConfig';
 
 import Routes from './routes';
+import { store, persistor } from './store';
 
-// import { Container } from './styles';
-
-export default function src() {
+export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar translucent backgroundColor="transparent" />
-      <Routes />
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar translucent backgroundColor="transparent" />
+        <NavigationContainer>
+          <Routes />
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 }
