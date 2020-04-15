f18502f02873ee1af7a8f9bfd5e403a6c7c8b736
afmelo92 <andre.fabian.melo@gmail.com>
2020-04-15 18:34:34 -0400

    Add Appointment exhibition and cancelation

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+
 import api from '~/services/api';
 
 import Background from '~/components/Background';
@@ -20,6 +21,21 @@ export default function Dashboard() {
     loadAppointments();
   }, []);
 
+  async function handleCancel(id) {
+    const response = await api.delete(`appointments/${id}`);
+
+    setAppointments(
+      appointments.map((appointment) =>
+        appointment.id === id
+          ? {
+              ...appointment,
+              canceled_at: response.data.canceled_at,
+            }
+          : appointment
+      )
+    );
+  }
+
   return (
     <Background>
       <Container>
@@ -28,7 +44,9 @@ export default function Dashboard() {
         <List
           data={appointments}
           keyExtractor={(item) => String(item.id)}
-          renderItem={({ item }) => <Appointment data={item} />}
+          renderItem={({ item }) => (
+            <Appointment onCancel={() => handleCancel(item.id)} data={item} />
+          )}
         />
       </Container>
     </Background>
